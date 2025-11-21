1c6b32f41ce3f3a7906d067cc29a18baeaf0b46d
Juan Gutierrez <jdgutierrez.2@sudamericano.edu.ec>
2025-11-21 12:12:28 -0500

    update proyect page

--- src/pages/ProyectPage.tsx
+++ src/pages/ProyectPage.tsx
@@ -1,14 +1,14 @@
 import Sidebar from "@/components/SideBar"
 import { Button } from "@/components/ui/button"
 import { useEffect, useState } from "react"
-import { Dialog, DialogClose, DialogContent,  DialogHeader, DialogTrigger, DialogFooter, DialogTitle } from "@/components/ui/dialog"
+import { Dialog, DialogClose, DialogContent,  DialogHeader, DialogTrigger, DialogFooter, DialogTitle, DialogDescription } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import axios from "axios"
 import { Loader2 } from "lucide-react"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { AlertCircleIcon, CheckCircle2Icon } from "lucide-react"
+import { AlertCircleIcon, CheckCircle2Icon, Search, Pencil, Trash } from "lucide-react"
 import { Label } from "@/components/ui/label"
-import { Search } from "lucide-react"
+
 
 type Proyect = {
   id: string
@@ -41,7 +41,7 @@ type Skill = {
 
 const ProyectPage = () => {
   const [projects, setProjects] = useState<Proyect[]>([])
-  const [proyectName, setProyectName] = useState("")
+  const [projectName, setProjectName] = useState("")
   const [description, setDescription] = useState("")
   const accessToken = localStorage.getItem("token")
   const [loadingProjects, setLoadingProjects] = useState(false)
@@ -54,6 +54,10 @@ const ProyectPage = () => {
   const userId=localStorage.getItem('id')
   const [skills, setSkills]=useState<Skill[]>([])
   const [search, setSearch] = useState("")
+  const [selectedProject, setSelectedProject] = useState<Proyect | null>(null);
+  const [deleteSuccess, setDeleteSuccess]=useState(false)
+  const [errorDelete, setErrorDelete]=useState(false)
+  
 
   const fetchProjects = async () => {
         setLoadingProjects(true)
@@ -68,7 +72,7 @@ const ProyectPage = () => {
         setLoadingProjects(false)
       }
 
-    const fetchSkills=async()=>{
+/*     const fetchSkills=async()=>{
       try {
         const res=await axios.get('localhost:8000/api/skills', {
           headers: {
@@ -79,7 +83,7 @@ const ProyectPage = () => {
       } catch (error) {
         console.log('Error al obtener las skills', error)
       }
-    }
+    } */
 
     useEffect(() => {
       const fetchCareers = async () => {
@@ -114,26 +118,60 @@ const ProyectPage = () => {
     fetchUser()
     }, [])
 
-/*   const fetchCareers=async()=>{
-    try {
-      const res= await axios.get('http://localhost:8000/api/careers', 
-        {headers: {
-          Authorization: `Bearer ${accessToken}`
-        }}
-      )
-      setCareers(res.data.data)
-      console.log('Carreras obtenidas', res.data.data)
-    } catch (error) {
-      console.log('Error al obtener las carreras')
+    const loadProjectData = (project: Proyect) => {
+        setSelectedProject(project);
+        setProjectName(project.name);
+        setDescription(project.description);
+        setCareerId(project.careerId);
+      };
+
+    const handleDeleteProyect=async(id: string)=>{
+      try {
+        setLoading(true)
+        await axios.delete(`http://localhost:8000/api/projects/${id}`)
+        setDeleteSuccess(true)
+        setTimeout(()=>{
+          setDeleteSuccess(false)
+        }, 3000)
+        await fetchProjects(  )
+      } catch (error) {
+        setErrorDelete(true)
+        console.log(error)
+        setTimeout(()=>{
+          setErrorDelete(false)
+        }, 3000)
+      }finally{
+        setLoading(false)
+      }
     }
-    fetchCareers()
-  } */
+
+      const handleEditProyect = async (id: string) => {
+        setLoading(true)
+        try {
+          setLoading(true);
+          await axios.patch(`http://localhost:8000/api/projects/${id}`, {
+            name: projectName,
+            description,
+            careerId,
+          });
+
+          setSuccess(true);
+          setTimeout(() => setSuccess(false), 3000);
+
+          await fetchProjects();
+        } catch (error) {
+          setErrorAlert(true);
+          setTimeout(() => setErrorAlert(false), 3000);
+        } finally {
+          setLoading(false);
+        }
+      };
 
     const handleCreateProyect=async()=>{
     try {
       const res=await axios.post('http://localhost:8000/api/projects',
         {
-          name: proyectName,
+          name: projectName,
           description,
           careerId
         }
@@ -158,7 +196,7 @@ const ProyectPage = () => {
       <div className="flex bg-gray-800 min-h-screen">
         <Sidebar />
 
-        <main className="flex-1 ml-0 md:ml-64 p-6 transition-all">
+        <main className="flex-1 ml-0 md:ml-64 p-6 transition-all w-screen">
           <div className="flex justify-between items-center mb-6">
             <h2 className="text-2xl font-black text-white">Panel General</h2>
               <div className="relative">
@@ -173,7 +211,7 @@ const ProyectPage = () => {
               </div>
             <Dialog>
               <DialogTrigger asChild>
-                <Button className="bg-cyan-600 hover:bg-cyan-700 text-white px-4 py-2 rounded-lg text-xs">
+                <Button className="bg-cyan-600 hover:bg-cyan-700 text-white px-4 py-2 rounded-lg text-xs cursor-pointer">
                   + Nuevo Proyecto
                 </Button>
               </DialogTrigger>
@@ -181,9 +219,6 @@ const ProyectPage = () => {
               <DialogContent className="sm:max-w-md bg-gray-800">
                 <DialogHeader>
                   <DialogTitle className="text-l font-black">Añadir Proyecto</DialogTitle>
-{/*                   <DialogDescription>
-                    Ingresa la información básica del nuevo proyecto.
-                  </DialogDescription> */}
                 </DialogHeader>
 
                 <div className="grid gap-4 py-4">
@@ -191,7 +226,7 @@ const ProyectPage = () => {
                   <Input
                     className="w-full px-3 py-2 rounded-md bg-gray-800 border border-gray-700 text-white"
                     placeholder="Nombre del proyecto"
-                    onChange={(e) => setProyectName(e.target.value)}
+                    onChange={(e) => setProjectName(e.target.value)}
                   />
                   <Input
                     className="w-full px-3 py-2 rounded-md bg-gray-800 border border-gray-700 text-white"
@@ -215,9 +250,9 @@ const ProyectPage = () => {
                 </select>
                 <DialogFooter>
                   <DialogClose asChild>
-                    <Button variant="ghost">Cancelar</Button>
+                    <Button variant="ghost" className="bg-red-600 cursor-pointer hover:bg-red-800">Cancelar</Button>
                   </DialogClose>
-                  <Button onClick={handleCreateProyect}>Crear</Button>
+                  <Button className="bg-green-600 cursor-pointer hover:bg-green-800" onClick={handleCreateProyect}>Crear</Button>
                 </DialogFooter>
               </DialogContent>
             </Dialog>
@@ -242,28 +277,127 @@ const ProyectPage = () => {
               .map(p => {
                   const careerName = careers.find(c => c.id === p.careerId)?.name;
                 return(
-                <div key={p.id} className="p-4 bg-gray-700 rounded-lg text-white">
-                  <h1><strong>Título:</strong> {p.name}</h1>
-                  <h2><strong>Descripción:</strong> {p.description}</h2>
-                  <h2><strong>Carrera:</strong> {careerName || "Sin carrera"}</h2>
-                </div>
+                <>
+                  <div
+                    key={p.id}
+                    className="p-4 bg-gray-900 rounded-lg text-white border border-gray-600 shadow-md"
+                  >
+                    <div className="flex justify-between items-start">
+                      <div className="space-y-1">
+                        <h1 className="font-semibold text-lg"><strong>Título:</strong> {p.name}</h1>
+                        <h2><strong>Descripción:</strong> {p.description}</h2>
+                        <h2><strong>Carrera:</strong> {careerName || "Sin carrera"}</h2>
+                      </div>
+                      <div className="flex flex-col items-center gap-2">
+                        <Dialog>
+                          <DialogTrigger asChild>
+                            <Button variant="ghost" className="p-2 bg-gray-700 hover:bg-gray-600 cursor-pointer">
+                              <Trash className="w-5 h-5 stroke-red-500" />
+                            </Button>
+                          </DialogTrigger>
+                          <DialogContent className="sm:max-w-md bg-gray-800">
+                            <DialogHeader>
+                              <DialogTitle>Eliminar Proyecto</DialogTitle>
+                            </DialogHeader>
+                            <DialogDescription>
+                              ¿Está seguro que desea eliminar el proyecto {p.name}?
+                            </DialogDescription>
+                            <DialogFooter>
+                              <DialogClose asChild>
+                                <Button className="bg-gray-600 hover:bg-gray-700 cursor-pointer">Cancelar</Button>
+                              </DialogClose>
+                              <Button
+                                className="bg-red-600 hover:bg-red-800 cursor-pointer"
+                                onClick={() => handleDeleteProyect(p.id)}
+                              >
+                                Eliminar
+                              </Button>
+                            </DialogFooter>
+                          </DialogContent>
+                        </Dialog>
+                        <Dialog>
+                          <DialogTrigger asChild>
+                            <Button
+                              onClick={() => loadProjectData(p)}
+                              variant="ghost"
+                              className="p-2 bg-gray-700 hover:bg-gray-600 cursor-pointer "
+                            >
+                              <Pencil className="w-5 h-5" />
+                            </Button>
+                          </DialogTrigger>
+
+                          <DialogContent className="sm:max-w-md bg-gray-800">
+                            <DialogHeader>
+                              <DialogTitle className="font-semibold text-lg">Editar Proyecto</DialogTitle>
+                            </DialogHeader>
+                            <div className="grid gap-3 py-3">
+                              <Label>Nombre del proyecto</Label>
+                              <Input
+                                className="bg-gray-900 border-gray-700 text-white"
+                                value={projectName}
+                                onChange={(e) => setProjectName(e.target.value)}
+                              />
+
+                              <Label>Descripción</Label>
+                              <Input
+                                className="bg-gray-900 border-gray-700 text-white"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                              />
+                            </div>
+
+                            <DialogFooter>
+                              <DialogClose asChild>
+                                <Button className="bg-red-600 hover:bg-red-800 cursor-pointer">Cancelar</Button>
+                              </DialogClose>
+                              <Button
+                                className="bg-green-600 hover:bg-green-800 cursor-pointer"
+                                onClick={() => handleEditProyect(p.id)}
+                              >
+                                Editar
+                              </Button>
+                            </DialogFooter>
+                          </DialogContent>
+                        </Dialog>
+                      </div>
+                    </div>
+                  </div>
+                </>
               )
               })}
                 {success && (
                   <Alert className="fixed top-4 right-4 w-auto bg-green-700 text-white">
                     <CheckCircle2Icon />
-                    <AlertTitle>El proyecto se ha creado correctamente!</AlertTitle>
+                    <AlertTitle>El proyecto se ha creado o editado correctamente!</AlertTitle>
                     <AlertDescription>
-                      El proyecto con nombre {proyectName} se ha creado correctamente!
+                      El proyecto con nombre {projectName} se ha creado o editado correctamente!
                     </AlertDescription>
                   </Alert>
                 )}
                 {errorAlert && (
                   <Alert className="fixed top-4 right-4 w-auto bg-red-700 text-white">
                     <AlertCircleIcon />
-                    <AlertTitle>Error al crear el proyecto</AlertTitle>
+                    <AlertTitle>Error al crear o editar el proyecto</AlertTitle>
                     <AlertDescription>
-                      Ha ocurrido un error al crear el proyecto, intente nuevamente
+                      Ha ocurrido un error al crea o editar el proyecto, intente nuevamente
+                    </AlertDescription>
+                  </Alert>
+                )}
+                {deleteSuccess && (
+                  <Alert className="fixed top-4 right-4 w-auto bg-green-700 text-white">
+                    <CheckCircle2Icon />
+                    <AlertTitle>El proyecto se ha eliminado correctamente!</AlertTitle>
+                    <AlertDescription>
+                      El proyecto seleccionado se ha eliminado correctamente!
+                    </AlertDescription>
+                  </Alert>
+                )}
+                {errorDelete && (
+                  <Alert className="fixed top-4 right-4 w-auto bg-red-700 text-white">
+                    <AlertCircleIcon />
+                    <AlertTitle>Error al eliminar el proyecto</AlertTitle>
+                    <AlertDescription>
+                      Ha ocurrido un error al eliminar el proyecto, intente nuevamente
                     </AlertDescription>
                   </Alert>
                 )}             
